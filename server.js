bc57e9590af39f56628434d82f2099191cc971cd
thisaruudam <thisaruudam@gmail.com>
2018-10-02 12:49:57 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/archi'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/archi/index.html'));
 });
     
 
